bd05bd67bbc9d5dc4004b3a4c9c3e561d2b23533
Andreas Elia <andreaselia@live.co.uk>
2024-04-13 15:29:08 +0100

    add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import type { LinksFunction } from "@remix-run/node";
+import stylesheet from "~/tailwind.css?url";
 import {
   Links,
   Meta,
@@ -5,6 +7,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
