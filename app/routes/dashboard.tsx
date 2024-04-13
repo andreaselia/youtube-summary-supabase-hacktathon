823e05767b49a28b49bc09ef1219028309655b10
Andreas Elia <andreaselia@live.co.uk>
2024-04-13 22:41:50 +0100

    loader fix

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,10 +1,25 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+
+import { createSupabaseServerClient } from "~/supabase.server";
 
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
     { name: "description", content: "Welcome to Remix!" },
   ];
+};
+
+export const loader = async ({ request }: ActionFunctionArgs) => {
+  const { supabaseClient } = createSupabaseServerClient(request);
+
+  const user = await supabaseClient.auth.getUser();
+
+  if (!user) {
+    return redirect("/sign-in");
+  }
+
+  return null;
 };
 
 export default function Dashboard() {
