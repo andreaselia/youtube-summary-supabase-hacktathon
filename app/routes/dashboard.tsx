fcd78faf0350fc16a6d5eb7c9e0d50f7ecfbef82
Andreas Elia <andreaselia@live.co.uk>
2024-04-15 11:11:54 +0100

    icon and spacing fixes

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -73,7 +73,7 @@ export default function Dashboard() {
   }, [actionResponse]);
 
   return (
-    <div className="mx-auto w-full max-w-screen-sm">
+    <div className="py-8 md:py-16 mx-auto w-full max-w-screen-sm">
       <div className="flex items-center justify-between">
         <h1 className="text-3xl font-bold">
           Dashboard
@@ -87,7 +87,7 @@ export default function Dashboard() {
           </button>
         </Form>
       </div>
-      <div className="mt-6 mx-auto w-full max-w-sm">
+      <div className="mt-8 mx-auto w-full max-w-sm">
         <Form ref={formRef} method="post" className="flex items-center space-x-2">
           <input
             name="video_url"
@@ -116,7 +116,7 @@ export default function Dashboard() {
                 <input type="hidden" name="video_id" value={video.id} />
                 <button type="submit" aria-label="Delete video">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
-                    <path d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z" />
+                    <path fillRule="evenodd" d="M5 3.25V4H2.75a.75.75 0 0 0 0 1.5h.3l.815 8.15A1.5 1.5 0 0 0 5.357 15h5.285a1.5 1.5 0 0 0 1.493-1.35l.815-8.15h.3a.75.75 0 0 0 0-1.5H11v-.75A2.25 2.25 0 0 0 8.75 1h-1.5A2.25 2.25 0 0 0 5 3.25Zm2.25-.75a.75.75 0 0 0-.75.75V4h3v-.75a.75.75 0 0 0-.75-.75h-1.5ZM6.05 6a.75.75 0 0 1 .787.713l.275 5.5a.75.75 0 0 1-1.498.075l-.275-5.5A.75.75 0 0 1 6.05 6Zm3.9 0a.75.75 0 0 1 .712.787l-.275 5.5a.75.75 0 0 1-1.498-.075l.275-5.5a.75.75 0 0 1 .786-.711Z" clipRule="evenodd" />
                   </svg>
                 </button>
               </Form>
