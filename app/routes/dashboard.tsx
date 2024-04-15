f53fdb3b014cdadf43881445997d5b43d16fa412
Andreas Elia <andreaselia@live.co.uk>
2024-04-15 18:38:56 +0100

    spacing

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -117,18 +117,8 @@ export default function Dashboard() {
                   key={video.id}
                   className="px-3 py-1.5 flex items-center justify-between rounded-lg odd:bg-gray-100"
                 >
-                  <div className="animate-pulse flex space-x-4">
-                    <div className="rounded-full bg-slate-700 h-10 w-10"></div>
-                    <div className="flex-1 space-y-6 py-1">
-                      <div className="h-2 bg-slate-700 rounded"></div>
-                      <div className="space-y-3">
-                        <div className="grid grid-cols-3 gap-4">
-                          <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                          <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                        </div>
-                        <div className="h-2 bg-slate-700 rounded"></div>
-                      </div>
-                    </div>
+                  <div className="animate-pulse">
+                    <div className="w-full h-3 bg-slate-700 rounded"></div>
                   </div>
                 </div>
               );
