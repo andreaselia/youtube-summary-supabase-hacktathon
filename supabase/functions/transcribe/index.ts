f154a4f02c5676fd133a1f60e6fba50e48b205a2
Andreas Elia <andreaselia@live.co.uk>
2024-04-16 00:23:29 +0100

    updates and cleanup

--- supabase/functions/transcribe/index.ts
+++ supabase/functions/transcribe/index.ts
@@ -1,9 +1,5 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-import OpenAI from 'https://deno.land/x/openai@v4.24.0/mod.ts'
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2"
+import OpenAI from "https://deno.land/x/openai@v4.24.0/mod.ts"
 
 import { fetchCaptions } from "../_lib/fetch-captions.ts"
 
@@ -11,13 +7,13 @@ console.log("Hello from Functions!")
 
 Deno.serve(async (req) => {
   const supabaseClient = createClient(
-    Deno.env.get('MY_SUPABASE_URL') ?? '',
-    Deno.env.get('MY_SUPABASE_SERVICE_ROLE_KEY') ?? ''
-    // { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+    Deno.env.get("MY_SUPABASE_URL") ?? "",
+    Deno.env.get("MY_SUPABASE_SERVICE_ROLE_KEY") ?? ""
+    // { global: { headers: { Authorization: req.headers.get("Authorization")! } } }
   )
 
   const openai = new OpenAI({
-    apiKey: Deno.env.get('OPENAI_API_KEY')!,
+    apiKey: Deno.env.get("OPENAI_API_KEY")!,
   })
 
   const { video } = await req.json()
@@ -27,7 +23,7 @@ Deno.serve(async (req) => {
   // eslint-disable-next-line no-useless-escape
   const regexp = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/gi
   const match = regexp.exec(video.video_url)
-  const videoId = match[1] // e.g. "dQw4w9WgXcQ"
+  const videoId = match ? match[1] : "dQw4w9WgXcQ"
 
   console.log("Fetching captions for video:", videoId)
 
@@ -35,7 +31,7 @@ Deno.serve(async (req) => {
 
   const chatCompletion = await openai.chat.completions.create({
     messages: [{
-      role: 'user',
+      role: "user",
       content: `
         Write a short summary of the video captions below.
         Include a section for the summary, key takeaways, highlights and keywords.
@@ -44,7 +40,7 @@ Deno.serve(async (req) => {
         Use H3 for the key takeaways, highlights and keywords.
         Captions: ${captions.subtitles}`,
     }],
-    model: 'gpt-3.5-turbo',
+    model: "gpt-3.5-turbo",
     stream: false,
   })
 
@@ -75,15 +71,3 @@ Deno.serve(async (req) => {
     { headers: { "Content-Type": "application/json" } },
   )
 })
-
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/transcribe' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
