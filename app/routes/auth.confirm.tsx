e7131c56383c02809d2a1dad6a4aca7a51a92fd2
Andreas Elia <andreaselia@live.co.uk>
2024-04-15 11:38:23 +0100

    method check

--- app/routes/auth.confirm.tsx
+++ app/routes/auth.confirm.tsx
@@ -3,6 +3,12 @@ import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 import { createSupabaseServerClient } from "~/supabase.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
+  if (request.method === "HEAD" || request.method === "OPTIONS") {
+    return new Response(null, {
+      status: 204,
+    });
+  }
+
   const url = new URL(request.url);
   const tokenHash = url.searchParams.get("token_hash");
 
