4842faf171144bf53c712e1f0c363574bcb7a6fe
Andreas Elia <andreaselia@live.co.uk>
2024-04-15 10:23:13 +0100

    index fix

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,11 +10,11 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div>
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-      <Link to="/dashboard">Go to dashboard</Link>
+      <Link to="/dashboard" className="mt-4 px-2 py-1 bg-gray-100 rounded-md text-sm font-semibold">Go to dashboard</Link>
     </div>
   );
 }
