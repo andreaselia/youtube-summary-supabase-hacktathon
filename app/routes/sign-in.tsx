1192ac01778a798ca3eb3208837d47c6d3713c4d
Andreas Elia <andreaselia@live.co.uk>
2024-04-15 20:52:52 +0100

    update message

--- app/routes/sign-in.tsx
+++ app/routes/sign-in.tsx
@@ -61,26 +61,26 @@ export default function SignIn() {
           Sign In
         </h1>
 
-        <div className="mt-4">
-          {!actionResponse?.success ? (
-            <Form method="post">
-              <input
-                name="email"
-                type="email"
-                placeholder="Your Email"
-                required
-                className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"
-              />
-              <button
-                type="submit"
-                className="mt-2 w-full bg-sky-500 hover:bg-sky-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white"
-              >
-                Sign in
-              </button>
-            </Form>
-          ) : (
+        <div className="mt-4 gap-y-2">
+          {actionResponse?.success && (
             <p>Please check your email for the sign in link.</p>
           )}
+
+          <Form method="post">
+            <input
+              name="email"
+              type="email"
+              placeholder="Your Email"
+              required
+              className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"
+            />
+            <button
+              type="submit"
+              className="mt-2 w-full bg-sky-500 hover:bg-sky-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white"
+            >
+              Sign in
+            </button>
+          </Form>
         </div>
       </div>
     </div>
