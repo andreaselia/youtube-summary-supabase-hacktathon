edadebf6bf0b36671c371f92f3ab228a7cffadfd
Andreas Elia <andreaselia@live.co.uk>
2024-04-20 17:14:25 +0100

    link to channel url

--- app/routes/videos.$video.tsx
+++ app/routes/videos.$video.tsx
@@ -82,7 +82,18 @@ export default function Dashboard() {
       <div className="mt-8 grid grid-cols-4 gap-x-4">
         <p className="flex-auto py-0.5 text-sm leading-5 text-gray-500 flex flex-col items-center">
           <span>Channel</span>
-          <span className="font-medium text-gray-900">{loaderResponse.channel}</span>
+          {loaderResponse.channel_url ? (
+            <a href={loaderResponse.channel_url} target="_blank" className="inline-flex items-center gap-x-0.5">
+              <svg width="24" height="24" fill="none" viewBox="0 0 24 24" className="w-4 h-4">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M16.75 13.25L18 12C19.6569 10.3431 19.6569 7.65685 18 6V6C16.3431 4.34315 13.6569 4.34315 12 6L10.75 7.25" />
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M7.25 10.75L6 12C4.34315 13.6569 4.34315 16.3431 6 18V18C7.65685 19.6569 10.3431 19.6569 12 18L13.25 16.75" />
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M14.25 9.75L9.75 14.25" />
+              </svg>
+              <span className="font-medium text-gray-900 underline">{loaderResponse.channel}</span>
+            </a>
+          ) : (
+            <span className="font-medium text-gray-900">{loaderResponse.channel}</span>
+          )}
         </p>
         <p className="flex-auto py-0.5 text-sm leading-5 text-gray-500 flex flex-col items-center">
           <span>Duration</span>
